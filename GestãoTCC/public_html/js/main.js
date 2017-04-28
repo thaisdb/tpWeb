af88a8adf0274ae11b9c26b30ad6d7d226acb62d
raphael <raphaelchagas_@hotmail.com>
2017-04-28 19:50:26 -0300

    - Fim do Formulário de Agendamento e mudanças no layout

--- GestãoTCC/public_html/js/main.js
+++ GestãoTCC/public_html/js/main.js
@@ -1,1 +1,9 @@
 
+let coorientador = document.getElementById('cb-coorientador');
+console.log(coorientador.checked);
+coorientador.addEventListener('click', function(){
+    if(coorientador.checked){
+        document.getElementById('box-coorientador').classList.remove('hidden');
+    }else document.getElementById('box-coorientador').classList.add('hidden');
+});
+
